89e25a4388a10009a357d725482ed2b8f60f80e8
Chang Min <2sounsin@gmail.com>
2023-04-18 11:55:10 +0900

    7week

--- src/stores/todo.js
+++ src/stores/todo.js
@@ -31,5 +31,22 @@ export default defineStore("useTodo", {
   
         LocalStorage.set("todo", this.tasks);
       },
+      removeTodo(id){
+        // 배열 안 오브젝트일때 idx
+        const idx = this.tasks.findIndex(task=>task.id == id);
+        //삭제 array.splice(시작 index, 제거 index, 추가 요소)
+        this.tasks.splice(idx,1);
+        LocalStorage.set("todo", this.tasks);
+      },
+      listTodo(){
+        this.tasks = LocalStorage.getItem("todo");
+    },
+    editTodo(item){
+        //배열에서 수정하되 done은 'n'으로
+        const idx = this.tasks.findIndex(task=>task == item);
+        item.done = 'N';
+        this.tasks.splice(idx,1,item); 
+        LocalStorage.set("todo", this.tasks);
+    }
   }
 })
